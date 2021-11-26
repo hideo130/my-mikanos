3690163cb66cb17cc9e488dbad44d6ae8cf42baa
Hideo Adachi <ckl15003@nitech.jp>
2021-11-26 23:16:09 +0900

    making interruptor

---
+++ kernel/interrupt.cpp
@@ -0,0 +1,11 @@
+#include <cstdint>
+#include <array>
+#include "interrupt.hpp"
+
+void NotifyEndOfInterrupt()
+{
+    volatile auto end_of_interrupt = reinterpret_cast<uint32_t *>(0xfee000b0);
+    *end_of_interrupt = 0;
+}
+
+std::array<InterruptDescriptor, 256> idt;
---
+++ kernel/interrupt.hpp
@@ -0,0 +1,36 @@
+#include <cstdint>
+
+enum class DescriptorType {
+  kUpper8Bytes   = 0,
+  kLDT           = 2,
+  kTSSAvailable  = 9,
+  kTSSBusy       = 11,
+  kCallGate      = 12,
+  kInterruptGate = 14,
+  kTrapGate      = 15,
+};
+
+
+union InterruptDescriptorAttribute
+{
+    uint16_t data;
+    struct
+    {
+        uint16_t interrupt_stack_table : 3;
+        uint16_t : 5;
+        DescriptorType type : 4;
+        uint16_t : 1;
+        uint16_t descriptor_privilege_level : 2;
+        uint16_t present : 1;
+    } __attribute__((packed)) bits;
+} __attribute((packed));
+
+struct InterruptDescriptor
+{
+    uint16_t offser_low;
+    uint16_t segment_selector;
+    InterruptDescriptorAttribute attr;
+    uint16_t offset_middle;
+    uint32_t offset_high;
+    uint32_t reserved;
+} __attribute__((packed));
