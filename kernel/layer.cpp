27c46d97b0d178110e39761b0024b42e1e34f07b
Hideo Adachi <ckl15003@nitech.jp>
2021-12-12 22:21:32 +0900

    we can drag window.
    But window is in some area,  displacement is collapsed.

--- kernel/layer.cpp
+++ kernel/layer.cpp
@@ -27,6 +27,12 @@ unsigned int Layer::ID() const
     return id_;
 }
 
+Layer &Layer::SetDraggable(bool draggable)
+{
+    draggable_ = draggable;
+    return *this;
+}
+
 Layer &Layer::SetWindow(const std::shared_ptr<Window> &window)
 {
     window_ = window;
@@ -41,6 +47,11 @@ Vector2D<int> Layer::GetPosition() const
 std::shared_ptr<Window> Layer::GetWindow() const
 {
     return window_;
+}
+
+bool Layer::IsDraggable() const
+{
+    return draggable_;
 }
 
 Layer &LayerManager::NewLayer()
@@ -115,6 +126,33 @@ void LayerManager::Draw(unsigned int id) const
         }
     }
     screen_->Copy(window_area.pos, back_bunffer_, window_area);
+}
+
+Layer *LayerManager::FindLayerByPosition(Vector2D<int> pos, unsigned int exclude_id) const
+{
+    auto pred = [pos, exclude_id](Layer *layer)
+    {
+        if (layer->ID() == exclude_id)
+        {
+            return false;
+        }
+        const auto &win = layer->GetWindow();
+        if (!win)
+        {
+            return false;
+        }
+        const auto win_pos = layer->GetPosition();
+        const auto win_end_pos = win_pos + win->Size();
+        return win_pos.x <= pos.x && pos.x < win_end_pos.x &&
+               win_pos.y <= pos.y && pos.y < win_end_pos.y;
+    };
+
+    auto it = std::find_if(layer_stack_.rbegin(), layer_stack_.rend(), pred);
+    if (it == layer_stack_.rend())
+    {
+        return nullptr;
+    }
+    return *it;
 }
 
 void LayerManager::Hide(unsigned int id)
--- kernel/layer.hpp
+++ kernel/layer.hpp
@@ -12,11 +12,13 @@ public:
     Layer(unsigned int id = 0);
     unsigned int ID() const;
 
+    Layer &SetDraggable(bool draggable);
     Layer &SetWindow(const std::shared_ptr<Window> &window);
 
     Vector2D<int> GetPosition() const;
     std::shared_ptr<Window> GetWindow() const;
 
+    bool IsDraggable() const;
     Layer &Move(Vector2D<int> pos);
     Layer &MoveRelative(Vector2D<int> pos_diff);
 
@@ -26,6 +28,7 @@ private:
     unsigned int id_;
     Vector2D<int> pos_;
     std::shared_ptr<Window> window_;
+    bool draggable_{false};
 };
 
 /**
@@ -41,6 +44,8 @@ public:
 
     void Draw(unsigned int id) const;
     void Draw(const Rectangle<int> &area) const;
+
+    Layer *FindLayerByPosition(Vector2D<int> mouse_position, unsigned int mouse_layer_id) const;
 
     /**
      * @brief Move layer position to specify absolute position
