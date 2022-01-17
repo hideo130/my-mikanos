c8fb9c4696a713c30e5529c4734ed904bf044ffc
Hideo Adachi <ckl15003@nitech.jp>
2022-01-17 23:46:30 +0900

    arrange Makefile for MacOS
    we can call rpn function.

--- kernel/interrupt.cpp
+++ kernel/interrupt.cpp
@@ -52,29 +52,41 @@ namespace
             {
                 x += '0';
             }
-            WriteAscii(*screen_writer, pos + Vector2D<int>{8 * i, 0}, x, {0, 0, 0});
+            WriteAscii(*screen_writer, pos + Vector2D<int>{8 * i, 0}, x, {255, 255, 255});
         }
+    }
+
+    void PrintFrame(InterruptFrame *frame, const char *exp_name)
+    {
+        WriteString(*screen_writer, {500, 16 * 0}, exp_name, {255, 255, 255});
+        WriteString(*screen_writer, {500, 16*1}, "CS:RIP", {255, 255, 255});
+        PrintHex(frame->cs, 4, {500 + 8 * 7, 16 * 1});
+        PrintHex(frame->rflags, 16, {500 + 8 * 12, 16 * 1});
+        WriteString(*screen_writer, {500, 16 * 2}, "RFLAGS", {255, 255, 255});
+        PrintHex(frame->rflags, 16, {500 + 8 * 7, 16 * 2});
+        WriteString(*screen_writer, {500, 16 * 3}, "SS:RSP", {255, 255, 255});
+        PrintHex(frame->ss, 4, {500 + 8 * 7, 16 * 3});
+        PrintHex(frame->rsp, 16, {500 + 8 * 12, 16 * 3});
     }
 
 #define FaultHandlerWithError(fault_name)                                                              \
     __attribute__((interrupt)) void IntHandler##fault_name(InterruptFrame *frame, uint64_t error_code) \
     {                                                                                                  \
         PrintFrame(frame, "#" #fault_name);                                                            \
-        WriteWtring(*screen_writer, {500 + 8 * 4, 16 * 4});                                            \
+        WriteString(*screen_writer, {500, 16 * 4}, "ERR", {255, 255, 255});                          \
         PrintHex(error_code, 16, {500 + 8 * 4, 16 * 4});                                               \
         while (true)                                                                                   \
             __asm__("hlt");                                                                            \
     }
 
-#define FaultHandlerNoError(fault_name)                                             \
-    __attribute__(((interrupt))) void IntHandler##fault_name(InterruptFrame *frame) \
-    {                                                                               \
-        PrintFrame(frame, "#", #fault_name);                                        \
-        while (true)                                                                \
-            __asm__("hlt");                                                         \
+#define FaultHandlerNoError(fault_name)                                           \
+    __attribute__((interrupt)) void IntHandler##fault_name(InterruptFrame *frame) \
+    {                                                                             \
+        PrintFrame(frame, "#" #fault_name);                                       \
+        while (true)                                                              \
+            __asm__("hlt");                                                       \
     }
 
-    FaultHandlerNoError(DE);
     FaultHandlerNoError(DE);
     FaultHandlerNoError(DB);
     FaultHandlerNoError(BP);
@@ -98,13 +110,32 @@ namespace
 
 void InitializeInterrupt()
 {
-    SetIDTEntry(idt[InterruptVector::kXHCI],
-                MakeIDTAttr(DescriptorType::kInterruptGate, 0),
-                reinterpret_cast<uint64_t>(IntHandlerXHCI),
-                kKernelCS);
-    SetIDTEntry(idt[InterruptVector::kLAPICTimer],
-                MakeIDTAttr(DescriptorType::kInterruptGate, 0),
-                reinterpret_cast<uint64_t>(IntHandlerLAPICTimer),
-                kKernelCS);
+    auto set_idt_entry = [](int irq, auto handler)
+    {
+        SetIDTEntry(idt[irq],
+                    MakeIDTAttr(DescriptorType::kInterruptGate, 0),
+                    reinterpret_cast<uint64_t>(handler),
+                    kKernelCS);
+    };
+    set_idt_entry(InterruptVector::kXHCI, IntHandlerXHCI);
+    set_idt_entry(InterruptVector::kLAPICTimer, IntHandlerLAPICTimer);
+    set_idt_entry(0, IntHandlerDE);
+    set_idt_entry(1, IntHandlerDB);
+    set_idt_entry(3, IntHandlerBP);
+    set_idt_entry(4, IntHandlerOF);
+    set_idt_entry(5, IntHandlerBR);
+    set_idt_entry(6, IntHandlerUD);
+    set_idt_entry(7, IntHandlerNM);
+    set_idt_entry(8, IntHandlerDF);
+    set_idt_entry(10, IntHandlerTS);
+    set_idt_entry(11, IntHandlerNP);
+    set_idt_entry(12, IntHandlerSS);
+    set_idt_entry(13, IntHandlerGP);
+    set_idt_entry(14, IntHandlerPF);
+    set_idt_entry(16, IntHandlerMF);
+    set_idt_entry(17, IntHandlerAC);
+    set_idt_entry(18, IntHandlerMC);
+    set_idt_entry(19, IntHandlerXM);
+    set_idt_entry(20, IntHandlerVE);
     LoadIDT(sizeof(idt) - 1, reinterpret_cast<uintptr_t>(&idt[0]));
 }
