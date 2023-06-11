67241d077314c263697dbc4e6e30080e9e73c96a
Hideo Adachi <ckl15003@nitech.jp>
2023-06-11 11:27:56 +0900

    imp day24g
    forgot to commit

--- kernel/interrupt.cpp
+++ kernel/interrupt.cpp
@@ -8,6 +8,7 @@
 #include "segment.hpp"
 #include "task.hpp"
 #include "timer.hpp"
+#include "signal.h"
 
 void NotifyEndOfInterrupt()
 {
@@ -65,9 +66,22 @@ namespace
         PrintHex(frame->rsp, 16, {500 + 8 * 12, 16 * 3});
     }
 
+    void KillApp(InterruptFrame *frame)
+    {
+        const auto cpl = frame->cs & 0x3;
+        if (cpl != 3)
+        {
+            return;
+        }
+        auto &task = task_manager->CurrentTask();
+        __asm__("sti");
+        ExitApp(task.OSStackPointer(), 128 + SIGSEGV);
+    }
+
 #define FaultHandlerWithError(fault_name)                                                              \
     __attribute__((interrupt)) void IntHandler##fault_name(InterruptFrame *frame, uint64_t error_code) \
     {                                                                                                  \
+        KillApp(frame);                                                                                \
         PrintFrame(frame, "#" #fault_name);                                                            \
         WriteString(*screen_writer, {500, 16 * 4}, "ERR", {255, 255, 255});                            \
         PrintHex(error_code, 16, {500 + 8 * 4, 16 * 4});                                               \
@@ -78,6 +92,7 @@ namespace
 #define FaultHandlerNoError(fault_name)                                           \
     __attribute__((interrupt)) void IntHandler##fault_name(InterruptFrame *frame) \
     {                                                                             \
+        KillApp(frame);                                                           \
         PrintFrame(frame, "#" #fault_name);                                       \
         while (true)                                                              \
             __asm__("hlt");                                                       \
